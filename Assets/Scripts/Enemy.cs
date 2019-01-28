2cf0e673f09cc55b69eeeb5214d2e67e8ef35e7b
Taylor <Taylorl@rogers.com>
2019-01-28 11:53:44 -0500

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    public GameObject player;
+    public int moveSpeed = 6;
+
+    void FixedUpdate(){
+
+        if (!player.GetComponent<Player>().paused)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, moveSpeed * Time.deltaTime);
+        }
+
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public int moveSpeed = 5;
+
+    public bool paused = true; 
+
+	void FixedUpdate () {
+
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            gameObject.transform.Translate(Vector2.left * Time.deltaTime * moveSpeed);
+            paused = false;
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            gameObject.transform.Translate(Vector2.right * Time.deltaTime * moveSpeed);
+            paused = false;
+        }
+        if (Input.GetAxis("Vertical") < 0)
+        {
+            gameObject.transform.Translate(Vector2.down * Time.deltaTime * moveSpeed);
+            paused = false;
+        }
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            gameObject.transform.Translate(Vector2.up * Time.deltaTime * moveSpeed);
+            paused = false;
+        }
+
+        if (Input.GetAxis("Horizontal") == 0 && Input.GetAxis("Vertical") == 0)
+            paused = true;
+    }
+}
